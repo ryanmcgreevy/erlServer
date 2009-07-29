a3e80ed443e1a1e36bced8e11128fb255462fcf4
Ryan McGreevy <ryan@T5400-1Ubuntu32.(none)>
2009-07-29 15:50:20 -0400

    Modified client handling, added in begginnings of XML-like text parsing and included packet design

--- socketTest.py
+++ socketTest.py
@@ -1,14 +1,19 @@
 import socket
+import thread
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 1800))
 
+def listen():
+    while 1:
+        r = s.recv(1024)
+        if r == "quit":
+            s.close()
+            break
+        else:
+            print r + "\n"
+
+thread.start_new_thread(listen, ())
 while 1:
     s.send(raw_input("message to send:"))
-    r = s.recv(1024)
-    if r == "quit":
-        s.close()
-        break
-    else:
-        print r
 
 
